98de4c31dc0e0c403a20de715874ca76fb341a3d
faisal <faisal.hadi@doku.com>
2019-08-15 10:24:30 +0700

    Add docker for DB Postgresql User

---
+++ docker-user-db/schema/20190528_01_001_001.sql
@@ -0,0 +1,68 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.4.4
+-- Dumped by pg_dump version 9.5.5
+
+-- Started on 2019-05-28 16:53:25 WIB
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+--  SET row_security = off;
+
+--
+-- TOC entry 1 (class 3079 OID 12123)
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+
+--
+-- TOC entry 2269 (class 0 OID 0)
+-- Dependencies: 1
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- TOC entry 174 (class 1259 OID 15944250)
+-- Name: inquiry; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE "USER" (
+  id uuid DEFAULT uuid_generate_v4() NOT NULL,
+  "name" varchar(256),
+  "username" varchar(256),
+  "password" varchar(256),
+  "email" varchar(256),
+  "phone_country_code" varchar(2),
+  "phone" varchar(256),
+  "creator_id" varchar,
+  "referrer_code" varchar(50),
+  "additional_data_1" varchar(256),
+  "additional_data_2" varchar(256),
+  "additional_data_3" varchar(256),
+  "additional_data_4" varchar(256),
+  "created_datetime" datetime,
+  "lastupdated_datetime" datetime,
+  "state" varchar(50),
+  "status" boolean
+);
+
+
+
